e9d356b085041d91455ca70a332e480a8ee61a7b
chriswranek <chriswranek@gmail.com>
2022-03-14 14:14:12 +0000

    Update AttributeMeasures.java

--- src/main/java/ml_6002b_coursework/AttributeMeasures.java
+++ src/main/java/ml_6002b_coursework/AttributeMeasures.java
@@ -6,5 +6,17 @@ package ml_6002b_coursework;
  */
 public class AttributeMeasures {
 
+    public static double measureInformationGain(int[] arr){
+
+
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 
 }
